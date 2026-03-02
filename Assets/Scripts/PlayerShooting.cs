6bd2bf406889c05165f5b652de89ac9125013ad9
ArildWennstrom <arild.wennstrom@elev.ga.lbs.se>
2026-03-02 18:39:33 +0100

    gjorde script så man kan skjuta

--- Assets/Scripts/PlayerShooting.cs
+++ Assets/Scripts/PlayerShooting.cs
@@ -2,15 +2,20 @@ using UnityEngine;
 
 public class PlayerShooting : MonoBehaviour
 {
+    [SerializeField] GameObject bulletPrefab;
+    [SerializeField] Transform firePoint;
+    [SerializeField] float bulletSpeed = 15f;
+    PlayerMovement pm;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        pm = GetComponent<PlayerMovement>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        pm.
     }
 }
